e23963da5b91c3c7beb104f9c5d9c7dddcd66c52
MAC <user@MACs-MBP.lan>
2022-03-30 11:22:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
